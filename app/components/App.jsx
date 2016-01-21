87452d30845d0a662c945ad697abe662cec4e718
Mark Cheng <chenghtmark@gmail.com>
2016-01-21 20:08:53 +0800

    before extraction of Note. Page 74

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,60 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
 
-export default class App extends React.Component {
-  render() {
-    return <Note />
+export default class App extends React.Component 
+{
+  constructor(props) {
+    super(props);
+    
+    this.addNote = this.addNote.bind(this);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
   }
+
+  render() {
+    const notes = this.state.notes;
+   
+    return (
+      <div>
+      <button onClick={this.addNote}>+</button>
+        <ul>{notes.map((note) =>
+          <li key={note.id}>{note.task}</li>
+          )}</ul>
+      </div>
+    );
+  }
+  addNote(state) {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New Task'
+      }])
+    }, () => console.log('set state!'));
+  };
+
 }
+  
+//   addNote = () => {
+//     this.setState({
+//       notes: this.state.notes.concat([{
+//         id: uuid.v4(),
+//         task: 'New Task'
+//       }])
+//     });
+//   };
+// }
